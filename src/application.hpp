b7af0c902e0950d096d140bb577ecef2e9c76464
Fabio <fabioscp0602@gmail.com>
2024-07-26 17:32:18 +0200

    Initial commit

---
+++ src/application.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <raylib.h>
+#include <rlights.h>
+#include <renderer.hpp>
+
+constexpr int g_ScreenWidth = 1600;
+constexpr int g_ScreenHeight = 900;
+constexpr char* g_WindowTitle = "RaylibFPS";
+
+constexpr int MAX_CUBES = 30;
+
+class Application{
+public:
+    Application();
+    ~Application();
+
+    void Run();
+private:
+    void Init();
+    void Deinit();
+
+    void HandleInputs();
+
+    Camera m_Camera;
+    Model m_Model;
+    Model m_Cube;
+    Shader m_GBufferShader;
+    Shader m_DeferredShader;
+    GBuffer m_GBuffer;
+    Light m_Lights[MAX_LIGHTS];
+    DeferredMode m_DeferredMode = DEFERRED_SHADING;
+    const float m_CubeScale = 0.25;
+    Vector3 m_CubePositions[MAX_CUBES];
+    float m_CubeRotations[MAX_CUBES];
+};
